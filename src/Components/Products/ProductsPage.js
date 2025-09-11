ea36653012d170451b6a181836432a9d888c0ca7
WalukaUI <cwaluka@yahoo.com>
2025-09-10 22:34:58 -0500

    added products to individual divs

--- src/Components/Products/ProductsPage.js
+++ src/Components/Products/ProductsPage.js
@@ -1,8 +1,17 @@
-
+import "./ProductsPage.css"
 function Products({products}) {
+    function showProducts(products) {
+        return products.map((item)=> <div id={item.id} className="itemBox">
+            <p>{item.title}</p>
+            <p>{item.price}</p>
+            <p>{item.discription}</p>
+            <p>{item.category}</p>
+            <button>Add to cart</button>
+        </div>)
+    }
     return <div className="products">
         <h3> List of products</h3>
-        {products.map((item)=> <a>{item.price}</a>)}
+        {showProducts(products)}
     </div>
 }
 

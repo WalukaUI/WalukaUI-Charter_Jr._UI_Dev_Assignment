985add716615a9df4b8983ec02c0bcc95ef937fa
WalukaUI <cwaluka@yahoo.com>
2025-09-11 20:19:21 -0500

    point functionality added

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,12 @@
-function Cart({totalPrice, removeitems}) {
+function Cart({totalPrice, removeItems, purchaseItems}) {
 
     return <>
         <div>
         <h3>{totalPrice.toFixed(2)}</h3>
         <a>click Remove Button to remove items from the cart</a><br/>
-        <button onClick={removeitems}>Remove</button>
+        <button onClick={removeItems}>Remove</button><br/>
+        <a>click Purchase Button to purchase the items in the cart</a><br/>
+        <button onClick={purchaseItems}>Purchase</button>
     </div>
     </>
 }
